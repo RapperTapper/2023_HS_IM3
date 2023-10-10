f85cbe5eed95cea8d47b086016b01b72d6b2a780
RapperTapper <raphael.schnell@stud.fhgr.ch>
2023-10-10 10:58:33 +0200

    go

--- 05_files/ex_2_showphotos.js
+++ 05_files/ex_2_showphotos.js
@@ -1,5 +1,12 @@
 import { supa } from "../../00_setup/supabase.js";
 
+//Vorgehen
+//1. Alle Photos aus der DB holen
+//2. Für jedes Photo die URL holen
+//3. Für jedes Photo eine IMG-Tag erstellen und in das HTML einfügen
+//4. Für jedes Photo eine Caption erstellen und in das HTML einfügen
+
+//Hilfsfunktion, um die URL für ein Photo zu holen
 async function getSignedUrl(filePath) {
     const { data, error } = await supa.storage.from('photos').createSignedUrl(filePath, 300);
     if (error) {
@@ -8,7 +15,7 @@ async function getSignedUrl(filePath) {
     }
     return data.signedURL;
 }
-
+//Hilfsfunktion, um die Photos zu holen und anzuzeigen
 async function fetchAndDisplayPhotos() {
     const { data, error } = await supa.from("Photos").select("url, caption");
     if (error) {
@@ -16,6 +23,8 @@ async function fetchAndDisplayPhotos() {
         return;
     }
     const photosContainer = document.getElementById('photosContainer');
+    
+    //Für jedes Photo die URL holen und ein IMG-Tag erstellen
     for (const photo of data) {
         const signedUrl = await getSignedUrl(photo.url);
         if (signedUrl) {
